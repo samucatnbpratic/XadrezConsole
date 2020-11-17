513d8a13b623161cd9040464df91bfa56dec242d
Samuel de Morais <samucatnbpratic@gmail.com>
2020-11-17 09:48:48 -0300

    projeto criado

---
+++ Jogos.XadrezConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jogos.XadrezConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
